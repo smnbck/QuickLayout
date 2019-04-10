4fad43f91cc753a944ce7d8fa9f5b15d2eb4abc5
Simon Back <simon.back@jamitlabs.com>
2019-04-10 14:56:08 +0200

    Implement package.swift file to add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:4.0.0
+//
+//  Package.swift
+//  QuickLayout
+//
+//  Created by Simon Back on 04/10/19.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "QuickLayout",
+    products: [
+        .library(
+            name: "QuickLayout",
+            targets: ["QuickLayout"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QuickLayout",
+            dependencies: []
+        )
+    ]
+)
